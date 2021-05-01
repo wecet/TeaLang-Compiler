164f22a4ee1d79a4bb8ee0f3a2990c2b566ba76b
Flinkoo <owenagius24@gmail.com>
2021-05-01 23:01:16 +0200

    Added Filing System

---
+++ Lexer/lexer.cpp
@@ -0,0 +1,36 @@
+#pragma clang diagnostic push
+#pragma ide diagnostic ignored "cert-err58-cpp"
+
+#include "lexer.h"
+
+std::regex lexer::letter("[A-Za-z]");
+std::regex lexer::number("[0-9]");
+std::regex lexer::printable("[\\x20-\\x7E]");
+
+lexer::lexer::lexer() = default;
+lexer::lexer::~lexer() = default;
+
+// -- Defining Expressions --
+bool lexer::isLetter(char argc){
+
+    std::string argv;
+    argv.push_back(argc);
+    return (std::regex_match(argv, letter));
+
+}
+
+bool lexer::isNumber(char argc){
+
+    std::string argv;
+    argv.push_back(argc);
+    return (std::regex_match(argv, number));
+
+}
+
+bool lexer::isPrintable(char argc){
+
+    std::string argv;
+    argv.push_back(argc);
+    return (std::regex_match(argv, printable));
+    
+}
---
+++ Lexer/lexer.h
@@ -0,0 +1,36 @@
+#ifndef LEXER
+#define LEXER 
+
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+#include <tuple>
+#include <regex>
+#include <vector>
+
+
+namespace lexer{
+
+        //Variables
+        extern std::regex letter;
+        extern std::regex number;
+        extern std::regex printable;
+
+        //Boolean Functions 
+        bool isLetter(char argc);
+        bool isNumber(char argc);
+        bool isPrintable(char argc);
+       
+    class lexer{
+
+    public:
+
+        lexer();
+        ~lexer();
+
+    };
+
+};
+
+#endif
