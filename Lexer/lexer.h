b0f8d0a1ae7e842bd6a84956f66576a5bf47a740
Owen Agius <73174341+Fliinko@users.noreply.github.com>
2021-05-02 20:40:17 +0200

    Update lexer.h

--- Lexer/lexer.h
+++ Lexer/lexer.h
@@ -21,12 +21,22 @@ namespace lexer{
         bool isLetter(char argc);
         bool isNumber(char argc);
         bool isPrintable(char argc);
-
         bool isPlus(char argc);
         bool isStar(char argc);
         bool isMinus(char argc);
         bool isEquals(char argc);
-       
+        bool isSpaceState(unsigned int state);
+        bool isNL(char argc);
+        bool isExlamation(char argc);
+        bool isPunct(char argc);
+        bool isSpace(char argc);
+        bool isQuotes(char argc);
+        bool isBackSlash(char argc);
+        bool isForwardSlash(char argc);
+        bool isGreaterLess(char argc);
+        bool isUnderscore(char argc);
+        bool isFullstop(char argc);
+
     class lexer{
 
     public:
