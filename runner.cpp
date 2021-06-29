a6d0e35dce0c58301f8bef0cb54a796c3a72e396
Owen Agius <73174341+Fliinko@users.noreply.github.com>
2021-06-29 13:24:59 +0200

    Preparing runner file

---
+++ runner.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+
+#include "Lexer/lexer.h"
+#include "Parser/parser.h"
+#include "Visitor/xmlvisitor.h"
+#include "Semantics/semantic.h"
+#include "Interpreter/interpreter.h"
+
+int main() {
+    
+}
