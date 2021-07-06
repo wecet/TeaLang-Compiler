49531c1891a40377580d4006768127f34932fa06
Owen Agius <73174341+Fliinko@users.noreply.github.com>
2021-07-06 16:13:52 +0200

    Interpreter Finished!

--- Interpreter/interpreter.cpp
+++ Interpreter/interpreter.cpp
@@ -251,7 +251,37 @@ void visitor::Interpreter::visit(parser::ASTReturnNode *return_node){
 }
 
 void visitor::Interpreter::visit(parser::ASTBlockNode *block_node){
+    //New Scope
+    scopes.push_back(new IScope());
 
+    //Check if Function Block
+    for(unsigned int x=0; x < params.size(); x++){
+        switch(args[x].first){
+            case parser::INT:
+                scopes.back()->declare(params[x], args[x].second.i);
+                break;
+            case parser::FLOAT:
+                scopes.back()->declare(params[x], args[x].second.f);
+                break;
+            case parser::BOOL:
+                scopes.back()->declare(params[x], args[x].second.b);
+                break;
+            case parser::STRING:
+                scopes.back()->declare(params[x], args[x].second.s);
+                break;
+        }
+    }
+
+    params.clear();
+    args.clear();
+
+    //Check Statemetns in Block
+    for(auto &argv : block_node -> argc){
+        argv -> accept(this);
+    }
+
+    //Close
+    scopes.pop_back();
 }
 
 void visitor::Interpreter::visit(parser::ASTIfNode *if_node){
@@ -278,7 +308,7 @@ void visitor::Interpreter::visit(parser::ASTWhileNode *while_node){
 }
 
 void visitor::Interpreter::visit(parser::ASTFunctionNode *function_node){
-    scopes.back()->declare(function_node->type, function_node->signature, function_node->funcblock);
+    scopes.back()->declare(function_node->type, function_node->params, function_node->funcblock);
 }
 
 void visitor::Interpreter::visit(parser::ASTLiteralNode<int> *literal_node){
@@ -310,6 +340,141 @@ void visitor::Interpreter::visit(parser::ASTLiteralNode<std::string> *literal_no
 }
 
 void visitor::Interpreter::visit(parser::ASTBinaryNode *binary_node){
+
+    std::string op = binary_node -> op;
+
+    binary_node -> left -> accept(this);
+
+    //Left Node
+    parser::VAR_TYPE rvartype = expression_type;
+    variable lcontent = expression_content;
+
+    //Right Node
+    binary_node -> right -> accept(this);
+    parser::VAR_TYPE lvartype = expression_type;
+    variable rcontent = expression_content;
+
+    variable temp;
+
+    if(op == "+" || op == "-" || op == "*" || op == "/"){
+        //INT
+        if(lvartype == parser::INT && rvartype == parser::INT){
+            expression_type = parser::INT;
+            if(op == "+"){
+                temp.i = lcontent.i + rcontent.i;
+            }
+            else if(op == "-"){
+                temp.i = lcontent.i - rcontent.i;
+            }
+            else if(op == "*"){
+                temp.i = lcontent.i - rcontent.i;
+            }
+            else if(op == "/"){
+                if(rcontent.i == 0){
+                    std::cout << "Cannot Divide By 0" << std::endl;
+                }
+                else{
+                    temp.i = lcontent.i/rcontent.i;
+                }
+            }
+
+    }
+    //FLOAT
+    else if(lvartype == parser::FLOAT || rvartype == parser::FLOAT){
+        expression_type = parser::FLOAT;
+        float left= lcontent.f;
+        float right= rcontent.f;
+
+        if(lvartype == parser::INT){
+            left = lcontent.i;
+        } 
+        else if(rvartype == parser::INT){
+            right = rcontent.i;
+        }
+
+        if(op == "+"){
+            temp.f = left+right;
+        }
+        else if(op == "-"){
+            temp.f = left-right;
+        }
+        else if(op == "*"){
+            temp.f = left*right;
+        }
+        else if(op == "/"){
+            if(right == 0){
+                std::cout << "Cannot Divide by 0" << std::endl;
+            }
+            else{
+                temp.f = left/right;
+            }
+        }
+
+    }
+    else{
+        expression_type = parser::STRING;
+        temp.s = lcontent.s + rcontent.s;
+    }
+    }
+    //BOOLEAN
+    else if(op == "and" || op == "or"){
+        expression_type = parser::BOOL;
+        if(op == "and"){
+            temp.b = lcontent.b && rcontent.b;
+        }
+        else if(op == "or"){
+            temp.b = lcontent.b || rcontent.b;
+        }
+        else{
+            //Shouldn't Get to Here
+            std::cout << "Shouldn't Be Here but Invalid Type" << std::endl;
+        }
+    }
+
+    //Comparison Operators
+    else{
+        expression_type = parser::BOOL;
+        if(lvartype == parser::BOOL){
+            temp.b = (op == "==") ? lcontent.b == rcontent.b : lcontent.b != rcontent.b;
+        }        
+        else if(lvartype == parser::STRING){
+            temp.s = (op == "==") ? lcontent.s == rcontent.s : lcontent.s != rcontent.s;
+        }
+
+        else{
+            float left = lcontent.f;
+            float right = rcontent.f;
+
+            if(lvartype == parser::INT){
+                left = lcontent.i;
+            }
+            else if(rvartype == parser::INT){
+                right = rcontent.i;
+            }
+
+            if(op == "=="){
+                temp.b = left == right;
+            }
+            else if(op == "!="){
+                temp.b = left != right;
+            }
+            else if(op == "<"){
+                temp.b = left < right;
+            }
+            else if(op == "<="){
+                temp.b = left <= right;
+            }
+            else if(op == ">"){
+                temp.b = left > right;
+            }
+            else if(op == ">="){
+                temp.b = left >= right;
+            }
+            
+        }
+    }
+
+    expression_content = temp;
 
 }
 
@@ -345,9 +510,26 @@ void visitor::Interpreter::visit(parser::ASTUnaryNode *unary_node){
 
 void visitor::Interpreter::visit(parser::ASTFunctionCallNode *function_call_node){
 
+    std::vector<parser::VAR_TYPE> sig;
+    std::vector<std::pair<parser::VAR_TYPE, variable>> arguments;
+
+    for(auto parameters : function_call_node -> signature){
+        parameters->accept(this);
+        sig.push_back(expression_type);
+        arguments.emplace_back(expression_type, expression_content);
+    }
+    for(auto arg : arguments){
+        this -> args.push_back(arg);
+    }
+
+    unsigned long temp;
+    for(temp = scopes.size() - 1; !scopes[temp]->already_referenced(function_call_node->functype, sig); temp++);
+
+    params = scopes[temp] -> names(function_call_node->functype, sig);
+    scopes[temp] -> block(function_call_node->functype, sig) -> accept(this);
 }
 
-std::pair<parser::VAR_TYPE, variable> Interpreter::expr(){
+std::pair<parser::VAR_TYPE, variable> visitor::Interpreter::expr(){
     return std::move(std::make_pair(expression_type, expression_content));
 };
 
